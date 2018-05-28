58d2d94df7a30c741e96029dcce6c7f01cb1398b
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-05-29 00:41:33 +0700

    Code SocketServer, tạo interface IChatServer và IChatClient

---
+++ SocketServer/IChatClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public interface IChatClient
+    {
+        void Disconnect();
+        EndPoint GetEndPoint();
+    }
+}
---
+++ SocketServer/IChatServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public interface IChatServer
+    {
+        void Start(string ip, int port);
+        IDictionary<string, IChatClient> Clients { get; }
+        void Stop();
+
+    }
+}

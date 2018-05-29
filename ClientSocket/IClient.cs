cf4b6975e5b9204ec8f2dfb3f77ab0a68c52a553
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-05-29 21:44:51 +0700

    client Socket

---
+++ ClientSocket/IClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+    public interface IClient
+    {
+        void Connect(string ip, int port);
+        void Disconnect();
+        event Action<string> OnNewRecieve;
+    }
+}
---
+++ ClientSocket/SimpleTcp/SimpleTCPClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleTCP;
+namespace ClientSocket.SimpleTcp
+{
+    public class SimpleTCPClient : IClient
+    {
+        private SimpleTCPClient _client;
+        public event Action<string> OnNewRecieve;
+
+        public void Connect(string ip, int port)
+        {
+            try
+            {
+                _client.Connect(ip, port);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        public void Disconnect()
+        {
+            _client.Disconnect();
+        }
+    }
+}

06405ca53d5cecae1fed1e56dc8d898270ebc74c
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-05-30 00:53:33 +0700

    code Image Coverter để chuyển đổi byte[] sang image và ngược lại

---
+++ ImageConverter/ImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageConverter
+{
+    public class ImageConverter
+    {
+        public static byte[] ConvertImageToByteArray(Image image)
+        {
+            MemoryStream stream = new MemoryStream();
+            image.Save(stream, image.RawFormat);
+            return stream.ToArray();
+        }
+
+        public static Image CovertByteArrayToImage(byte[] imageArray)
+        {
+            MemoryStream ms = new MemoryStream(imageArray);
+            Image image = Image.FromStream(ms);
+            return image;
+        }
+    }
+}

644dfc7df5e68bb98a556b8af52fe1db87aaa3d7
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-06-04 23:24:06 +0700

    Handling Get list friends

---
+++ ChatProtocol/Handle/GetListFriendsRequestHandle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatProtocol.Protocol;
+using SocketServer;
+using ChatDataModel;
+using ChatDAO;
+
+namespace ChatProtocol.Handle
+{
+    public class GetListFriendsRequestHandle : IHandle
+    {
+        private object syncLock = new object();
+        public string Handling(IProtocol protocol, IChatClient client)
+        {
+            var ptc = protocol as GetListFriendsRequestProtocol;
+            string toView = "[" + DateTime.Now + "] [" + client.GetEndPoint() + "] [user:" + ptc.Email + "] get list friends ";
+            var listFriends = ListFriends(ptc.Email);
+            if (listFriends.Count == 0)
+            {
+                toView += "\n list friend is empty";
+                return toView;
+            }
+            client.ResponseGetListFriends(listFriends);
+            return toView += "\n get list friend successful ";
+        }
+
+        private List<Account> ListFriends(string email)
+        {
+            lock (syncLock)
+            {
+                IAccountDAO db = new AccountDAOSQL();
+                return db.GetFriendList(email);
+            }
+        }
+    }
+}

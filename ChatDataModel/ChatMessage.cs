4174bdc4d5d2557f634137e6e999432f84daa1d2
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-06-06 23:00:19 +0700

    Add DataModel ChatMessage

---
+++ ChatDataModel/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDataModel
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Message { get; set; }
+        public string ImageMessageDriveID { get; set; }
+        public DateTime TimeSend { get; set; }
+    }
+}

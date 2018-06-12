11e3416247e7668af076ec549a026a36eeb301be
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-06-12 15:19:55 +0700

    Insert Chat Group Message

---
+++ ChatDAO/SQL/ChatGroupMessageDAOSQL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatDataModel;
+using Database;
+using Database.SQLServer;
+
+namespace ChatDAO.SQL
+{
+    public class ChatGroupMessageDAOSQL : IChatGroupMessageDAO
+    {
+        IDatabase con = new SqlServerDB();
+
+        public void Connect()
+        {
+            con.Connect(@"Data Source=MINHDUC\SQLEXPRESS;Initial Catalog=ChatDB;Persist Security Info=True;User ID=sa;Password=123456");
+        }
+        public int Insert(ChatGroupMessage message)
+        {
+            try
+            {
+                Connect();
+                string sql = "INSERT INTO ChatGroupMessage VALUES ('" + message.Sender + "','" + message.GroupReceive + "',N'" + message.Message + "','" + message.ImageMessageDriveId + "','" + message.TimeSend + "')";
+                return con.ExecuteNonQuery(sql);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Disconnect();
+            }
+        }
+    }
+}
---
+++ ChatDataModel/ChatGroupMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDataModel
+{
+    public class ChatGroupMessage
+    {
+        public string Id { get; set; }
+        public string Sender { get; set; }
+        public string GroupReceive { get; set; }
+        public string Message { get; set; }
+        public string ImageMessageDriveId { get; set; }
+        public DateTime TimeSend { get; set; }
+
+        public ChatGroupMessage()
+        {
+
+        }
+
+        public ChatGroupMessage(string sender, string groupReceive, string  message, string imageMessageDriveId, DateTime timeSend)
+        {
+            Sender = sender;
+            GroupReceive = groupReceive;
+            Message = message;
+            ImageMessageDriveId = imageMessageDriveId;
+            TimeSend = timeSend;
+        }
+    }
+}

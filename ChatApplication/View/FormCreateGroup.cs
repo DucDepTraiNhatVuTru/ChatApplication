328cb0de3542be44f4a793adf7427d3785b6da4d
DucDepTraiNhatVuTru <31755740+DucDepTraiNhatVuTru@users.noreply.github.com>
2018-06-15 17:19:01 +0700

    code form Create Group

--- ChatApplication/View/FormCreateGroup.cs
+++ ChatApplication/View/FormCreateGroup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatDataModel;
+using ClientSocket;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telerik.WinControls.UI;
 
 namespace ChatApplication.View
 {
     public partial class FormCreateGroup : Form
     {
+        private IClient _client;
+        private List<Account> _listFriend = new List<Account>();
+
         public FormCreateGroup()
         {
             InitializeComponent();
         }      
+
+        private void InitListFriend()
+        {
+            _radLVFriends.AllowEdit = false;
+            _radLVFriends.AllowRemove = false;
+            _radLVFriends.ItemSize = new Size(_radLVFriends.Size.Width, 35);
+            _radLVFriends.ShowCheckBoxes = true;
+            lock (this)
+            {
+                _listFriend = Util.Instance.ListFriends;
+            }
+            LoadListFriend(_listFriend);
+        }
+
+        private void LoadListFriend(List<Account> accounts)
+        {
+            BindingList<Account> listUser = new BindingList<Account>();
+            foreach (var item in accounts)
+            {
+                listUser.Add(item);
+            }
+            _radLVFriends.DataSource = listUser;
+            _radLVFriends.DisplayMember = "Name";
+            _radLVFriends.ValueMember = "Id";
+        }
     }
 }
